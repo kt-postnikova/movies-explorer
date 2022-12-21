aa0efd14ff0163bc87fde882322aa51a31af5714
kt-postnikova <kt-postnikova@yandex.ru>
2022-12-21 12:55:39 +0300

    Mongoose error is fixed

--- api/app.js
+++ api/app.js
@@ -23,7 +23,8 @@ app.use(cors({
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 
-mongoose.connect(DB_URL, { useNewUrlParser: true });
+mongoose.set('strictQuery', false);
+mongoose.connect(DB_URL);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
